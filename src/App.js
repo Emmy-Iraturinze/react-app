41a1a38a86419c940d72e8ca5f9fc2770fc716d9
Emmy-Iraturinze <iraturinzeemmyy@gmail.com>
2021-08-01 13:44:31 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 
 import './App.css';
-import { Button,Card,Navbar,form } from "react-bootstrap"
+import { Button,Card,Navbar,form} from "react-bootstrap"
+import * as Icon from 'react-bootstrap-icons';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -22,21 +24,22 @@ function App() {
   </div>
   <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+  
+  <img src={require('../src/images/LOGO.png').default}  alt=".."/> 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a id="home" class="nav-link active" aria-current="page" href="#">Home</a>
+          <a id="home" class="nav-link active" aria-current="page" href="#">What We Offer</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Who we are</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href  id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+         Team
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             <li><a class="dropdown-item" href >Action</a></li>
@@ -46,7 +49,7 @@ function App() {
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href tabindex="-1" aria-disabled="true">Disabled</a>
+          <a class="nav-link " href="tel:+250782251506" ><i class="bi bi-telephone"></i>+250782251506</a>
         </li>
       </ul>
    
@@ -59,20 +62,34 @@ function App() {
   <div class="row">
 
     <div class="col">
-    <h1 class="text-white display-2" >We do E-Commerce.</h1>
+    <h1 class="text-white display-2" >We do E-Commerce<small>.</small></h1>
+    <p class="text-white">Save up to <span>50%</span>from working with us and your will Thank us Later. <br/>
+    We are Helping more Than 200 companies around the globe.</p>
     </div>
+    </div>
+    <form>
     <div class="row">
-    <div class="col">
-  <input style={{marginLeft:"268px"}} type="text" class="form-control w-50" placeholder="Company Name"/>
-    </div>
-    </div>
-    <div class="row">
-    <div class="col">
-    <input type="submit" class="btn btn-danger w-50" />
-      </div>
-      </div>
-  </div>
 
+   
+<div class="col text-center">
+ 
+  <input  type="text" class="placehol" placeholder="Company Name"/>
+  
+
+</div>
+
+</div>
+<div class="row">
+    <div class="col">
+    <input type="submit" class="btn btn-danger w-25" />
+      </div>
+    
+      </div>
+    
+    </form>
+ 
+   
+ 
   </div>
 
 
